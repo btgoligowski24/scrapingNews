d673a3b26490e7751cf8655ff3a4eefb16ae1040
Ben Goligowski <51453763+btgoligowski24@users.noreply.github.com>
2019-11-10 21:49:24 -0600

    updating port variable

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const logger = require("morgan");
 const path = require("path");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Initialize Express
 const app = express();
